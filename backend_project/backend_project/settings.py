731226643e0b5ef5e2418a1e9d4e2a72c5152c73
Muhammadjon Victus <muhammadjonxamidov593@gmail.com>
2026-04-23 12:34:05 +0500

    third commit

--- backend_project/backend_project/settings.py
+++ backend_project/backend_project/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my apps
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -62,12 +65,15 @@ WSGI_APPLICATION = 'backend_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "diplom_backend",
+        "USER": "postgres",
+        "PASSWORD": "7982",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
