8061354428b81d74dc357e8882a59f0d671b36ec
Muhammadjon Victus <muhammadjonxamidov593@gmail.com>
2026-04-29 16:22:32 +0500

    7- commit

--- backend_project/login/views.py
+++ backend_project/login/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def home(request):
     return render(request, "index.html")
+
+
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username")
@@ -15,19 +18,13 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect("home")  # index pagega yo‘naltiradi
+            return redirect("home")
         else:
             messages.error(request, "Username yoki password noto‘g‘ri!")
 
     return render(request, "login.html")
 
-from django.shortcuts import render
-
 
 def logout_view(request):
     logout(request)
     return redirect("login")
-
-@login_required
-def home(request):
-    return render(request, "index.html")
